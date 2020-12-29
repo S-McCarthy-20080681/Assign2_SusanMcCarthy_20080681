24e78ea1f0b2c86302287e3d8cdb709b103d4c67
Susan McCarthy <susie.mccarthy98@gmail.com>
2020-12-29 14:28:36 +0000

    Assignment 2 Mobile App

---
+++ app/src/main/java/org/astro/assign2_susanmccarthy_20080681/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.astro.assign2_susanmccarthy_20080681
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
