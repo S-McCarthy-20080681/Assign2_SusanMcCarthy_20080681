e4b7e98b368e9f9f012c51b8a58190c384aabeb8
Susan McCarthy <susie.mccarthy98@gmail.com>
2020-12-31 19:19:42 +0000

    AstroAdapter has been refactored to tidy up code.

--- app/src/main/java/org/astro/assign2_susanmccarthy_20080681/activities/AstroListActivity.kt
+++ app/src/main/java/org/astro/assign2_susanmccarthy_20080681/activities/AstroListActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_astro_list.*
-import kotlinx.android.synthetic.main.card_astro.view.*
 import org.astro.assign2_susanmccarthy_20080681.activities.*
 import org.astro.assign2_susanmccarthy_20080681.main.MainApp
-import org.astro.assign2_susanmccarthy_20080681.models.AstroModel
 import org.jetbrains.anko.startActivityForResult
 
 class AstroListActivity : AppCompatActivity() {
@@ -42,32 +39,3 @@ class AstroListActivity : AppCompatActivity() {
     }
 
 }
-
-//class AstroAdapter constructor(private var astroList: List<AstroModel>) :
-//        RecyclerView.Adapter<AstroAdapter.MainHolder>() {
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-//        return MainHolder(
-//                LayoutInflater.from(parent.context).inflate(
-//                        R.layout.card_astro,
-//                        parent,
-//                        false
-//                )
-//        )
-//    }
-//
-//    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-//        val astroEvent = astroList[holder.adapterPosition]
-//        holder.bind(astroEvent)
-//    }
-//
-//    override fun getItemCount(): Int = astroList.size
-//
-//    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//
-//        fun bind(astroEvent: AstroModel) {
-//            itemView.astroEventTitle.text = astroEvent.title
-//            itemView.astroEventDescription.text = astroEvent.description
-//        }
-//    }
-//}
